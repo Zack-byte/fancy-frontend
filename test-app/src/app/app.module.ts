6d269040b48e35b76c48c69d0ee25928b89bbdf2
Zack <zacharyharris27@gmail.com>
2021-07-07 19:11:04 -0400

    started frameworking for adding app stores

--- test-app/src/app/app.module.ts
+++ test-app/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HighlighterTestComponent } from './highlighter-test/highlighter-test.component';
-import { HomePageComponent } from './home-page/home-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HighlighterTestComponent,
     HomePageComponent
   ],
   imports: [
