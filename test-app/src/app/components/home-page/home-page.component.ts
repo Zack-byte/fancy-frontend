a83963de4e749ab2fe056c018b2b2e4bf449aa04
Zachary Harris <zacharyharris27@gmail.com>
2021-08-09 16:44:02 -0400

    Merge branch 'main' of https://github.com/Zack-byte/Fancy-Frontend

--- test-app/src/app/components/home-page/home-page.component.ts
+++ test-app/src/app/components/home-page/home-page.component.ts
@@ -1,14 +1,31 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
+  animations: [
+    //----- Animations -----//
+    trigger('slideInLeft', [
+      transition(':enter', [
+        style({transform: 'translateX(-100%)'}),
+        animate('1000ms ease-in', style({transform: 'translateX(0%)'}))
+      ]),
+    ]),
+
+    trigger('slideInRight', [
+      transition(':enter', [
+        style({transform: 'translateX(100%)'}),
+        animate('1000ms ease-in', style({transform: 'translateX(0%)'}))
+      ]),
+    ]),
+  ]
 })
 export class HomePageComponent implements OnInit {
 
-  public englishTitle = 'Welcome | '
-  public japaneseTitle = ' | 歓迎'
+  public englishTitle = 'Hello | '
+  public japaneseTitle = ' | 你好'
 
   constructor() { }
 
