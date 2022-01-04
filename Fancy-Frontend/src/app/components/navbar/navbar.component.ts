52adc984cb958e6f9a052070ac3098d584cc0449
Zack <zacharyharris27@gmail.com>
2022-01-04 00:14:09 -0500

    #fixed navbar

--- Fancy-Frontend/src/app/components/navbar/navbar.component.ts
+++ Fancy-Frontend/src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { trigger, style, state, transition, animate } from '@angular/animations';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-navbar',
@@ -40,18 +41,52 @@ export class NavbarComponent implements OnInit {
 
 
   public goToHome(): void {
-    console.log('gone home');
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#home-page").scrollTop
+    }, 2000);
+    this.resetButtons();
+    $("#home").css("background-color", "rgb(8, 40, 158)");
+    $("#home").children().css("color", "white");
+    $('#home').css("box-shadow", "inset 1px 1px 10px #333")
   }
 
   public goToAbout(): void {
-    console.log('gone to about');
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#home-page").scrollTop
+    }, 2000);
+    this.resetButtons();
+    $("#about").css("background-color", "rgb(8, 40, 158)");
+    $("#about").children().css("color", "white");
+    $('#about').css("box-shadow", "inset 1px 1px 10px #333")
   }
 
   public goToProjects(): void {
-    console.log('gone to projects');
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#home-page").scrollTop
+    }, 2000);
+    this.resetButtons();
+    $("#projects").css("background-color", "rgb(8, 40, 158)");
+    $("#projects").children().css("color", "white");
+    $('#projects').css("box-shadow", "inset 1px 1px 10px #333")
+  }
+
+  public goToContact(): void {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#home-page").scrollTop
+    }, 2000);
+    this.resetButtons();
+    $("#contact").css("background-color", "rgb(8, 40, 158)");
+    $("#contact").children().css("color", "white");
+    $('#contact').css("box-shadow", "inset 1px 1px 10px #333")
   }
 
   public toggleNavbar(): void {
     this.showNav = !this.showNav;
+  }
+
+  public resetButtons(): void {
+    $('.nav-button').css("background-color", "white");
+    $('.nav-button').children().css("color", "rgb(8, 40, 158)");
+    $('.nav-button').css("box-shadow", "none")
   }
 }
